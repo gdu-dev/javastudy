30cd2fdf02fa5f46c4fb4dcdf99736195e132e85
gdu-dev <minkt78@naver.com>
2023-01-25 17:38:13 +0900

    2023-01-25

--- 09_Practice/src/practice/MainClass.java
+++ 09_Practice/src/practice/MainClass.java
@@ -1,6 +1,8 @@
 package practice;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 
 public class MainClass {
@@ -27,17 +29,46 @@ public class MainClass {
 	
 	// 문제2. C:\Program Files\Java\jdk-11.0.17 경로의 파일 목록을 아래와 같이 출력하시오.
 	/*
-	2023-01-04  오후 02:19    <DIR>          bin
-	2023-01-04  오후 02:19    <DIR>          conf
-	2023-01-04  오후 02:19    <DIR>          include
-	2023-01-04  오후 02:19    <DIR>          jmods
-	2023-01-04  오후 02:19    <DIR>          legal
-	2023-01-04  오후 02:19    <DIR>          lib
-	2023-01-04  오후 02:19               160 README.html
-	2023-01-04  오후 02:19             1,302 release
-	               2개 파일               1,462 바이트
+		2023-01-04  오후 02:19    <DIR>          bin
+		2023-01-04  오후 02:19    <DIR>          conf
+		2023-01-04  오후 02:19    <DIR>          include
+		2023-01-04  오후 02:19    <DIR>          jmods
+		2023-01-04  오후 02:19    <DIR>          legal
+		2023-01-04  오후 02:19    <DIR>          lib
+		2023-01-04  오후 02:19               160 README.html
+		2023-01-04  오후 02:19             1,302 release
+		               2개 파일               1,462 바이트
 	*/
 	public static void ex02() {
+		
+		File dir = new File("C:" + File.separator + "Program Files" + File.separator + "Java" + File.separator + "jdk-11.0.17");
+		
+		if(dir.exists()) {
+			
+			File[] list = dir.listFiles();
+			
+			int fileCount = 0;
+			long totalFileSize = 0;
+			for(File file : list) {
+				if(file.isHidden()) {
+					continue;
+				}
+				String lastModifiedDate = new SimpleDateFormat("yyyy-MM-dd  a hh:mm").format(file.lastModified());
+				String directory = file.isDirectory() ? "<DIR>" : "";
+				String size = "";
+				if(file.isFile()) {
+					long length = file.length();
+					size = new DecimalFormat("#,##0").format(length);
+					fileCount++;
+					totalFileSize += length;
+				}
+				String name = file.getName();
+				String result = String.format("%s%9s%9s %s\n", lastModifiedDate, directory, size, name);
+				System.out.print(result);
+			}
+			System.out.println(fileCount + "개 파일 " + new DecimalFormat("#,##0").format(totalFileSize) + " 바이트");
+			
+		}
 		
 	}
 	
@@ -45,19 +76,21 @@ public class MainClass {
 	// 파일이 저장된 디렉터리는 지워지지 않으므로 먼저 디렉터리에 저장된 파일을 삭제해야 한다.
 	public static void ex03() {
 		
+		File dir = new File("C:" + File.separator + "storage");
+		
+		File file = new File(dir, "myfile.txt");
+		if(file.exists()) {
+			file.delete();
+		}
+		
+		if(dir.exists()) {
+			dir.delete();
+		}
+		
 	}
 	
 	public static void main(String[] args) {
-		ex02();
+		ex03();
 	}
 
 }
-
-
-
-
-
-
-
-
-
