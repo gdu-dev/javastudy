5dc5f1671fee298f78f024d2fde1e255cc74c3ec
gdu-dev <minkt78@naver.com>
2023-01-26 17:25:40 +0900

    2023-01-26

--- 09_IO/src/ex02_Writer/JSONMainClass.java
+++ 09_IO/src/ex02_Writer/JSONMainClass.java
@@ -1,5 +1,14 @@
 package ex02_Writer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -122,23 +131,46 @@ public class JSONMainClass {
 		}
 		
 	}
+
+	public static void ex05() {
+		
+		Map<String, Object> product1 = new HashMap<String, Object>();
+		product1.put("model", "세탁기");
+		product1.put("maker", "삼성");
+		product1.put("price", 100);
+		
+		Map<String, Object> product2 = new HashMap<String, Object>();
+		product2.put("model", "냉장고");
+		product2.put("maker", "LG");
+		product2.put("price", 200);
+
+		Map<String, Object> product3 = new HashMap<String, Object>();
+		product3.put("model", "TV");
+		product3.put("maker", "삼성");
+		product3.put("price", 300);
+		
+		List<Map<String, Object>> products = Arrays.asList(product1, product2, product3);
+		
+		JSONArray arr = new JSONArray(products);
+		String content = arr.toString();
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		
+		File file = new File(dir, "product.json");
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+			bw.write(content);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public static void main(String[] args) {
-		ex04();
+		ex05();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
