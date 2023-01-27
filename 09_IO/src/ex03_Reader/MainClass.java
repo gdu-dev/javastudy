76d2a2cd31460835c7e47359eccddc66640fdd88
gdu-dev <minkt78@naver.com>
2023-01-27 12:25:14 +0900

    2023-01-27

--- 09_IO/src/ex03_Reader/MainClass.java
+++ 09_IO/src/ex03_Reader/MainClass.java
@@ -28,9 +28,14 @@ public class MainClass {
 			
 			fr = new FileReader(file);
 			
-			int c = fr.read();
+			int c;  // 1글자를 저장할 변수
 			
-			System.out.println((char)c);
+			StringBuilder sb = new StringBuilder();
+			while((c = fr.read()) != -1) {
+				sb.append((char)c);
+			}
+			
+			System.out.println(sb.toString());
 			
 		} catch(IOException e) {  // FileNotFoundException이 함께 처리된다.
 			e.printStackTrace();
@@ -46,8 +51,59 @@ public class MainClass {
 		
 	}
 	
+	public static void ex02() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		
+		File file = new File(dir, "ex02.txt");
+		
+		FileReader fr = null;
+		
+		try {
+			
+			fr = new FileReader(file);
+			
+			char[] cbuf = new char[5];  // 5글자를 읽어 들이는 배열
+			int readCount = 0;  // 실제로 읽은 글자의 개수
+			StringBuilder sb = new StringBuilder();
+			
+			while((readCount = fr.read(cbuf)) != -1) {
+				
+				sb.append(cbuf, 0, readCount);  // 인덱스 0부터 readCount개 글자를 sb에 추가하시오.
+				
+				// ex02.txt 읽는 과정
+				// 루프  readCount    cbuf
+				// 1     5            a b c d e
+				// 2     2            f g c d e
+				// 3     -1
+				
+				// for(int i = 0; i < readCount; i++) {
+				//	   System.out.print(cbuf[i]);
+				// }
+				
+			}  // while
+			
+			System.out.println(sb.toString());
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fr != null) {
+					fr.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		ex01();
+		ex02();
 	}
 
 }
