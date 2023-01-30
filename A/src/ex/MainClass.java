f3e9175aa1f876298762b86f9761449e9dca53ad
gdu-dev <minkt78@naver.com>
2023-01-30 18:50:40 +0900

    2023-01-30

--- A/src/ex/MainClass.java
+++ A/src/ex/MainClass.java
@@ -1,7 +1,11 @@
 package ex;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class MainClass {
 
@@ -31,8 +35,93 @@ public class MainClass {
 		
 	}
 	
+	public static void ex02() {
+		
+		List<String> list1 = Arrays.asList("봄", "여름", "가을", "겨울");
+		List<Integer> list2 = Arrays.asList(10, 20, 30, 40);
+		List<Car> list3 = Arrays.asList(
+				new Car("벤츠", 100), 
+				new Car("BMW", 200)
+		);
+		
+		// list1.add("암튼계절");  데이터 추가 불가
+		
+		System.out.println(list1);
+		System.out.println(list2);
+		System.out.println(list3);
+		
+	}
+
+	public static void ex03() {
+		
+		List<String> list = Arrays.asList("봄", "여름", "가을", "겨울");
+		
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+		System.out.println(list.get(3));
+		
+		System.out.println(list.size());
+		
+		for(int i = 0, length = list.size(); i < length; i++) {
+			System.out.println(list.get(i));
+		}
+		for(String str : list) {
+			System.out.println(str);
+		}
+		
+		List<Car> list3 = Arrays.asList(
+				new Car("벤츠", 100), 
+				new Car("BMW", 200)
+		);
+		
+		for(int i = 0, length = list3.size(); i < length; i++) {
+			System.out.println(list3.get(i));
+		}
+		for(Car car : list3) {
+			System.out.println(car);
+		}
+		
+	}
+	
+	public static void ex04() {
+		
+		Map<Integer, Car> map = new HashMap<Integer, Car>();
+		
+		// 추가
+		map.put(1, new Car("벤츠", 100));
+		map.put(2, new Car("BMW", 200));
+		
+		// Value 확인
+		System.out.println(map.get(1));  // Key가 1인 Value 가져오기
+		System.out.println(map.get(2));  // Key가 2인 Value 가져오기
+		
+	}
+	
+	public static void ex05() {
+		
+		Map<Integer, Car> map = new HashMap<Integer, Car>();
+		
+		map.put(1, new Car("벤츠", 100));
+		map.put(2, new Car("BMW", 200));
+		
+		for(Map.Entry<Integer, Car> entry : map.entrySet()) {
+			System.out.println("Key는" + entry.getKey() + "이고, Value는 " + entry.getValue() + "이다.");
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		ex01();
+		ex05();
 	}
 
 }
+
+
+
+
+
+
+
+
+
