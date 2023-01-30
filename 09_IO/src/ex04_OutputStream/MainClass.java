bc78aef7964f8969aad67be646022eba1ca354c7
gdu-dev <minkt78@naver.com>
2023-01-30 09:33:33 +0900

    2023-01-30

---
+++ 09_IO/src/ex04_OutputStream/MainClass.java
@@ -0,0 +1,57 @@
+package ex04_OutputStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainClass {
+
+	public static void ex01() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex01.bin");
+		
+		FileOutputStream fos = null;
+		
+		try {
+			
+			fos = new FileOutputStream(file);
+			
+			// 출력 단위
+			// 1. int    : 1개
+			// 2. byte[] : 2개 이상
+			
+			int c = 'A';
+			String str = "pple";
+			byte[] b = str.getBytes();
+			
+			// 출력
+			fos.write(c);
+			fos.write(b);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fos != null) {
+					fos.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		ex01();
+	}
+
+}
+
+
+
+
+
+
+
