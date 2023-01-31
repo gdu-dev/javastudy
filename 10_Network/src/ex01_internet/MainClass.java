3840584db7c0d682f2160cb360b9ab628a1cdf5b
gdu-dev <minkt78@naver.com>
2023-01-31 12:26:20 +0900

    2023-01-31

--- 10_Network/src/ex01_internet/MainClass.java
+++ 10_Network/src/ex01_internet/MainClass.java
@@ -4,7 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -147,34 +149,52 @@ public class MainClass {
 	public static void ex04() {
 		
 		String apiURL = "https://gdlms.cafe24.com/uflist/curri/10004/bbs/68_63d8848f7d506.txt";
+		URL url = null;
+		HttpURLConnection con = null;
+		
+		InputStreamReader reader = null;
+		FileWriter writer = null;
+		File file = new File("C:" + File.separator + "storage", "다운로드문서.txt");
+		
+		try {
+			
+			url = new URL(apiURL);
+			con = (HttpURLConnection) url.openConnection();
+			
+			int responseCode = con.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				reader = new InputStreamReader(con.getInputStream());
+			} else {
+				reader = new InputStreamReader(con.getErrorStream());
+			}
+			
+			StringBuilder sb = new StringBuilder();
+			char[] cbuf = new char[2];
+			int readCount = 0;
+			
+			while((readCount = reader.read(cbuf)) != -1) {
+				sb.append(cbuf, 0, readCount);
+			}
+			
+			writer = new FileWriter(file);
+			writer.write(sb.toString());
+			
+			writer.close();
+			reader.close();
+			con.disconnect();
+			
+			System.out.println("다운로드 완료");
+			
+		} catch(MalformedURLException e) {
+			System.out.println("apiURL 주소 오류");
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 	public static void main(String[] args) {
-		ex03();
+		ex04();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
