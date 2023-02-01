aafdbb7237a2317d8b3221a56cd0bc03f2b61e55
gdu-dev <minkt78@naver.com>
2023-02-01 11:48:07 +0900

    2023-02-01

---
+++ 10_Network/src/ex03_api/JSONMainClass.java
@@ -0,0 +1,61 @@
+package ex03_api;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class JSONMainClass {
+
+	public static void ex01() {
+		
+		String serviceKey = "bEQBRPHjt0tZrc7EsL0T8usfsZ1+wT+5jqamBef/ErC/5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ==";
+		String apiURL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
+		URL url = null;
+		HttpURLConnection con = null;
+		BufferedReader reader = null;
+		
+		try {
+			
+			String sidoName = "서울";
+			StringBuilder sbURL = new StringBuilder();
+			sbURL.append(apiURL);
+			sbURL.append("?serviceKey=" + URLEncoder.encode(serviceKey, "UTF-8"));
+			sbURL.append("&returnType=json");
+			sbURL.append("&sidoName=" + URLEncoder.encode(sidoName, "UTF-8"));
+			
+			url = new URL(sbURL.toString());
+			con = (HttpURLConnection) url.openConnection();
+			
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			
+			if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			
+			String line = null;
+			StringBuilder sb = new StringBuilder();
+			while((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			
+			reader.close();
+			con.disconnect();
+			
+			System.out.println(sb.toString());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		ex01();
+	}
+
+}
