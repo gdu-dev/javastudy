127cb7c87f6d096b99a2515549732a985f174b6a
gdu-dev <minkt78@naver.com>
2023-02-01 17:11:44 +0900

    2023-02-01

--- 06_Practice/src/practice02/MainClass.java
+++ 06_Practice/src/practice02/MainClass.java
@@ -1,5 +1,8 @@
 package practice02;
 
+import java.security.SecureRandom;
+import java.util.Scanner;
+
 public class MainClass {
 
 	// 문제1. 통장(balance)에 최초 5000원이 있다.
@@ -13,7 +16,13 @@ public class MainClass {
 	// 출금 전 18원, 5회 출금액 17원, 출금 후 1원
 	// 출금 전 1원, 6회 출금액 1원, 출금 후 0원
 	public static void ex01() {
-		
+		int balance = 5000;  // 통장
+		int count = 0;
+		while(balance > 0) {
+			count++;
+			int withdraw = (int)(Math.random() * balance) + 1;
+			System.out.println("출금 전 " + balance + "원, " + count + "회 출금액 " + withdraw + "원, 출금 후 " + (balance -= withdraw) + "원");
+		}
 	}
 	
 	// 문제2. UpDown 게임
@@ -28,7 +37,27 @@ public class MainClass {
 	// 입력 >>> 4500
 	// 정답. 총 5번만에 성공.
 	public static void ex02() {
-		
+		Scanner sc = new Scanner(System.in);
+		int target = (int)(Math.random() * 10000) + 1;
+		int guess;
+		int times = 0;
+		do {
+			System.out.print("입력 >>> ");
+			guess = sc.nextInt();
+			times++;
+			if (guess < 1 || guess > 10000) {
+				System.out.println("1~10000 사이만 유효합니다.");
+				continue;
+			}
+			if (guess == target) {
+				System.out.println("정답! 총 " + times + "번만에 성공.");
+			} else if (guess > target) {
+				System.out.println("Down!");
+			} else {
+				System.out.println("Up!");
+			}
+		} while(guess != target);
+		sc.close();
 	}
 	
 	// 문제3. 자동으로 진행되는 윷 던지기를 구현하시오. 윷이나 모가 나오면 추가로 던지시오.
@@ -39,7 +68,19 @@ public class MainClass {
 	// "윷", "도", 5칸 이동한다.
 	// "모", "윷", "도", 10칸 이동한다.
 	public static void ex03() {
-		
+		String[] yut = {"", "도", "개", "걸", "윷", "모"};
+		int totalMove = 0;
+		while (true) {
+			int move = (int)(Math.random() * 5) + 1;
+			System.out.print("\"" + yut[move] + "\"");  // "도"~"모" 출력
+			totalMove += move;
+			if (move <= 3) {
+				System.out.println(", " + totalMove + "칸 이동한다.");
+				break;
+			} else {
+				System.out.print(", ");
+			}
+		}
 	}
 	
 	// 문제4. 0~9 사이 난수를 100개 생성하시오.
@@ -56,7 +97,20 @@ public class MainClass {
 	// 8 : ####### 7
 	// 9 : ########### 11
 	public static void ex04() {
-		
+		int[] arr = new int[100];
+		int[] count = new int[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int)(Math.random() * 10);
+			count[arr[i]]++;
+		}
+		for (int i = 0; i < count.length; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < count[i]; j++) {
+				sb.append("#");
+			}
+			String graph = sb.toString();
+			System.out.println(i + " : " + graph + " " + count[i]);
+		}
 	}
 	
 	// 문제5. 간단한 성적 관리 프로그램을 구현하시오.
@@ -81,7 +135,32 @@ public class MainClass {
 	// 상철 70   71   73   214	
 	// 합계 180  183  189  552	
 	public static void ex05() {
-		
+		Scanner sc = new Scanner(System.in);
+		String[] name = {"정숙", "미희", "상철", "합계"};
+		String[] course = {"국어", "영어", "수학", "총점"};
+		int[][] score = new int[name.length][course.length];
+		for (int i = 0; i < score.length - 1; i++) {
+			for (int j = 0; j < score[i].length - 1; j++) {
+				System.out.print(name[i] + "의 " + course[j] + " 점수 >>> ");
+				score[i][j] = sc.nextInt();
+				score[i][course.length - 1] += score[i][j];
+				score[name.length - 1][j] += score[i][j];
+				score[name.length - 1][course.length - 1] += score[i][j];
+			}
+		}
+		System.out.print("\n\t");
+		for (int i = 0; i < course.length; i++) {
+			System.out.print(course[i] + "\t");
+		}
+		System.out.println();
+		for (int i = 0; i < score.length; i++) {
+			System.out.print(name[i] + "\t");
+			for (int j = 0; j < score[i].length; j++) {
+				System.out.print(score[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		sc.close();
 	}
 	
 	// 문제6. 4계절이 저장되어 있는 영한 사전(2차원 배열)을 이용하여 문제를 해결하시오.
@@ -95,7 +174,23 @@ public class MainClass {
 	// 겨울을 영어로 하면? >>> win
 	// 오답
 	public static void ex06() {
-		
+		Scanner sc = new Scanner(System.in);
+		String[][] dictionary = {
+				{"봄", "spring"},
+				{"여름", "summer"},
+				{"가을", "fall"},
+				{"겨울", "winter"}
+		};
+		for (int i = 0; i < dictionary.length; i++) {
+			System.out.print(dictionary[i][0] + "을 영어로 하면? >>> ");
+			String answer = sc.next();
+			if (answer.equalsIgnoreCase(dictionary[i][1])) {
+				System.out.println("정답");
+			} else {
+				System.out.println("오답");
+			}
+		}
+		sc.close();
 	}
 	
 	// 문제7. 다음 순서에 따라서 5 x 5 형태의 숫자 빙고판을 자동으로 생성하시오.
@@ -118,7 +213,28 @@ public class MainClass {
 	//  16 22 18 24 23
 
 	public static void ex07() {
-		
+		final int SIZE = 5;
+		int[][] bingo = new int[SIZE][SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				bingo[i][j] = (i * SIZE) + (j + 1);
+			}
+		}	
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				int x = (int)(Math.random() * SIZE);
+				int y = (int)(Math.random() * SIZE);
+				int temp = bingo[i][j];
+				bingo[i][j] = bingo[x][y];
+				bingo[x][y] = temp;
+			}
+		}
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				System.out.print(String.format("%3d", bingo[i][j]));
+			}
+			System.out.println();
+		}
 	}
 	
 	// 문제8. 대문자와 소문자와 숫자로 구성된 인증번호를 만드시오.
@@ -128,7 +244,24 @@ public class MainClass {
 	// 몇 자리의 인증번호를 생성할까요? >>> 6
 	// 생성된 6자리 인증번호는 Fa013b입니다.
 	public static void ex08() {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("몇 자리의 인증번호를 생성할까요? >>> ");
+		int count = sc.nextInt();
+		SecureRandom secureRandom = new SecureRandom();
+		StringBuilder sbCode = new StringBuilder();
+		for(int n = 0; n < count; n++) {
+			double randNumber = secureRandom.nextDouble();
+			if(randNumber < 0.33) {
+				sbCode.append((char)((int)(secureRandom.nextDouble() * 26) + 'A'));
+			} else if(randNumber < 0.66) {
+				sbCode.append((char)((int)(secureRandom.nextDouble() * 26) + 'a'));
+			} else {
+				sbCode.append((char)((int)(secureRandom.nextDouble() * 10) + '0'));
+			}
+		}
+		String code = sbCode.toString();
+		System.out.println("생성된 " + count + "자리 인증번호는 " + code + "입니다.");
+		sc.close();
 	}
 	
 	// 문제9. 다음 지시시항에 따라 요일을 계산하는 프로그램을 구현하시오.
@@ -154,7 +287,35 @@ public class MainClass {
 	// 년-월-일 입력(2000-01-01) >>> 2023-01-19
 	// 입력된 2023-01-19는 목요일입니다.
 	public static void ex09() {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("년-월-일 입력(2000-01-01) >>> ");
+		String date = sc.next();
+		String[] dateArr = date.split("-");
+		int year = Integer.parseInt(dateArr[0]);
+		int month = Integer.parseInt(dateArr[1]);
+		int day = Integer.parseInt(dateArr[2]);
+		int totalDays = 0;
+		// 년도를 totalDays에 누적
+		for(int y = 1; y < year; y++) {
+			totalDays += 365;
+			if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+				totalDays++;
+			}
+		}
+		// 월을 totalDays에 누적
+		int[] lastDays = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+		if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			lastDays[2] = 29;
+		}
+		for(int i = 0; i < month; i++) {
+			totalDays += lastDays[i];
+		}
+		// 일을 totalDays에 누적
+		totalDays += day;
+		// 결과
+		String[] weekName = {"일", "월", "화", "수", "목", "금", "토"};
+		System.out.println("입력된 " + date + "는 " + weekName[totalDays % 7] + "요일입니다.");
+		sc.close();
 	}
 	
 	// 문제10. 사용자가 입력한 금액만큼 로또를 구매한다고 가정하고, 완성된 로또 용지를 출력하시오.
@@ -183,7 +344,43 @@ public class MainClass {
 	// 01 :   13  10  42  34  32  21
 	// ------------------------------
 	public static void ex10() {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("얼마나 구입하시겠습니까? >>> ");
+		int money = sc.nextInt();
+		while(money > 0) {
+			int row = (money >= 5000) ? 5 : money / 1000;
+			int[][] lotto = new int[row][6];
+			for (int i = 0; i < lotto.length; i++) {
+				// 45개 숫자에서 랜덤하게 빼서 lotto[i][j]에 저장
+				int ballCount = 45;
+				int[] ball = new int[ballCount];
+				// 1~45 순차적으로 생성
+				for (int j = 0; j < ball.length; j++) {
+					ball[j] = j + 1;
+				}
+				// ball 배열에서 6개를 lotto[i][j] 보내기
+				for (int j = 0; j < lotto[i].length; j++) {
+					int rnd = (int)(Math.random() * ballCount);  // ball 배열의 인덱스(0~ballCount-1) 랜덤 생성
+					lotto[i][j] = ball[rnd];
+					int lastIdx = 44 - j;
+					if (rnd != lastIdx) {
+						ball[rnd] = ball[lastIdx];
+					}
+					ballCount--;
+				}
+			}
+			// 출력
+			for (int i = 0; i < lotto.length; i++) {
+				System.out.print("0" + (i + 1) + " : ");
+				for (int j = 0; j < lotto[i].length; j++) {
+					System.out.print( String.format("%4d", lotto[i][j]) );
+				}
+				System.out.println();
+			}
+			System.out.println("------------------------------");
+			money -= 5000;
+		}
+		sc.close();
 	}
 	
 	public static void main(String[] args) {
