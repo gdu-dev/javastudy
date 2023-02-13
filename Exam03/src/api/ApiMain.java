59081560cb7f406d4bd97df8edae15d7645d6cda
gdu-dev <minkt78@naver.com>
2023-02-13 18:06:25 +0900

    2023-02-13

---
+++ Exam03/src/api/ApiMain.java
@@ -0,0 +1,11 @@
+package api;
+
+public class ApiMain {
+
+	public static void main(String[] args) {
+		
+		String apiURL = "http://apis.data.go.kr/B552061/AccidentDeath/getRestTrafficAccidentDeath";
+
+	}
+
+}
