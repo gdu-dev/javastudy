08ca640804d624b184f881d7a7b0cd40f439c433
gdu-dev <minkt78@naver.com>
2024-01-04 17:15:52 +0900

    20240104

---
+++ 04_control/src/pkg03_branch_practice/MainClass.java
@@ -0,0 +1,41 @@
+package pkg03_branch_practice;
+
+public class MainClass {
+
+  public static void method1() {
+    
+    // 월에 따른 계절 출력하기
+    int month = 1;
+    
+    
+  }
+  
+  public static void method2() {
+    
+    // 점수와 학년에 따른 합격 여부
+    // 1~3학년 : 60점 이상이면 합격, 아니면 불합격
+    // 4~6학년 : 70점 이상이면 합격, 아니면 불합격
+    
+    int score = 60;  // 점수
+    int grade = 1;   // 학년
+    
+    
+  }
+  
+  public static void method3() {
+    
+    // 4일은 목요일, n일 후는 무슨 요일?
+    
+    int day = 4;      // 4일은 목요일
+    int nDay = 10;    // nDay일 후
+    String weekName;  // 요일 (월~일)
+    
+    
+  }
+  
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
