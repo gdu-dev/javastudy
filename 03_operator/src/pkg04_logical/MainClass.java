0209ec891b904499185beffdf78f8d0215ffcae2
gdu-dev <minkt78@naver.com>
2024-01-04 15:30:24 +0900

    20240104

--- 03_operator/src/pkg04_logical/MainClass.java
+++ 03_operator/src/pkg04_logical/MainClass.java
@@ -14,30 +14,38 @@ public class MainClass {
      */
     
     /*
-     * 숏 서킷
+     * 숏 서킷(Short Circuit)
      * 1. 연산 결과의 빠른 처리를 위한 동작이다.
      * 2. 종류
      *    1) && : false 가 반환되면 최종 결과도 false 이므로 더 이상 동작하지 않는다.
      *    2) || : true 가 반환되면 최종 결과도 true 이므로 더 이상 동작하지 않는다.
      */
     
-    boolean a = true && true;
-    boolean b = true && false;
-    boolean c = false && true;
-    boolean d = false && false;
+    int n1 = 10;
+    int n2 = 20;
+    
+    boolean a = n1 == 10 && n2 == 20;
+    boolean b = n1 == 10 && n2 == 10;
+    boolean c = n1 == 20 && ++n2 == 20;  // 숏 서킷에 의해서 ++n2 == 20 코드는 실행되지 않는다.
+    boolean d = n1 == 20 && n2++ == 20;  // 숏 서킷에 의해서 n2++ == 20 코드는 실행되지 않는다.
+    
     System.out.println(a);
     System.out.println(b);
     System.out.println(c);
     System.out.println(d);
     
     
+    boolean f = n1 == 10 || n2++ == 20;  // 숏 서킷에 의해서 n2++ == 20 코드는 실행되지 않는다.
+    boolean g = n1 == 10 || ++n2 == 10;  // 숏 서킷에 의해서 ++n2 == 20 코드는 실행되지 않는다.
     
-    
-    
-    
-    
+    System.out.println(f);
+    System.out.println(g);
     
 
+    boolean h = !(n1 == 10);
+    System.out.println(h);
+
+    
   }
 
 }
