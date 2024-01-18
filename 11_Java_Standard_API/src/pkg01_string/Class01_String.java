1790387f581e4a53d342fa305e5ac5a4ddfb5b4b
gdu-dev <minkt78@naver.com>
2024-01-18 12:48:33 +0900

    20240118

--- 11_Java_Standard_API/src/pkg01_string/Class01_String.java
+++ 11_Java_Standard_API/src/pkg01_string/Class01_String.java
@@ -1,5 +1,7 @@
 package pkg01_string;
 
+import java.util.Arrays;
+
 public class Class01_String {
 
   public static void method1() {
@@ -135,17 +137,111 @@ public class Class01_String {
     
   }
   
+  public static void method9() {
+    
+    // startsWith : 전달한 문자열로 시작하면 true 반환
+    // endsWith   : 전달한 문자열로 끝나면   true 반환
+    // contains   : 전달한 문자열을 포함하면 true 반환
+    
+    String str = "admin@example.com";
+    
+    System.out.println(str.startsWith("."));
+    System.out.println(str.endsWith(".com"));
+    System.out.println(str.contains("@"));
+    
+  }
   
+  public static void method10() {
+    
+    // isEmpty : 글자수가 0이면 true 반환
+    // isBlank : 공백 문자이면  true 반환 (jdk 11 부터 사용 가능)
+    
+    String str = " ";
+    
+    System.out.println(str.isEmpty());  // false
+    System.out.println(str.isBlank());  // true
+    
+  }
   
+  public static void method11() {
+    
+    // trim : 문자열의 앞뒤에 포함된 공백 문자 제거
+    
+    String str = "\n hello \n";
+    
+    System.out.println(str.trim().length());
+    
+  }
   
+  public static void method12() {
+    
+    // replace    : 어떤 문자열을 다른 문자열로 변환한 문자열을 반환
+    // replaceAll : 어떤 정규식 패턴을 다른 문자열로 변환한 문자열을 반환
+    
+    String str = "192.168.0.214";
+    
+    System.out.println(str.replace(".", "_"));
+    System.out.println(str.replaceAll("\\.", "_"));
+    
+  }
   
+  public static void method13() {
+    
+    // split : 지정한 정규식 패턴으로 문자열을 분리한 String[] 을 반환
+    // join  : String[] 의 모든 요소와 전달한 문자열을 연결한 문자열을 반환
+    
+    String str = "aaa.bbb.ccc.ddd.txt";
+    
+    String[] result = str.split("\\.");
+    System.out.println(Arrays.toString(result));
+    
+    System.out.println(result[result.length - 1]);
+    
+    System.out.println(String.join(".", result));
+    
+  }
   
+  public static void method14() {
+    
+    // toUpperCase : 대문자로 변환
+    // toLowerCase : 소문자로 변환
+    
+    String str = "I am a Student";
+    
+    System.out.println(str.toUpperCase());
+    System.out.println(str.toLowerCase());
+    
+  }
   
-  
-  
+  public static void method15() {
+    
+    // format : 형식이 적용된 문자열 반환
+    
+    // 실수 형식
+    double number1 = 100.5;
+    System.out.println(String.format("%f", number1));
+    System.out.println(String.format("%.2f", number1));
+    System.out.println(String.format("%10.2f", number1));
+    
+    // 정수 형식
+    int number2 = 123;
+    System.out.println(String.format("%o", number2));    // 8진수
+    System.out.println(String.format("%x", number2));    // 16진수(0~f)
+    System.out.println(String.format("%X", number2));    // 16진수(0~F)
+    System.out.println(String.format("%d", number2));    // 10진수
+    System.out.println(String.format("%5d", number2));   // 10진수
+    System.out.println(String.format("%05d", number2));  // 10진수
+    
+    // 문자열 형식
+    String str = "hello";
+    System.out.println(String.format("%s", str));
+    System.out.println(String.format("%10s", str));   // 오른쪽 정렬
+    System.out.println(String.format("%-10s", str));  // 왼쪽 정렬
+    
+  }
   
   public static void main(String[] args) {
-    practice01();
+    method15();
   }
 
   public static void practice01() {
