42ceb7600f3398f762d196776221e81cfa551808
gdu-dev <minkt78@naver.com>
2024-01-18 11:46:41 +0900

    20240118

--- 11_Java_Standard_API/src/pkg01_string/Class01_String.java
+++ 11_Java_Standard_API/src/pkg01_string/Class01_String.java
@@ -72,8 +72,98 @@ public class Class01_String {
     
   }
   
-  public static void main(String[] args) {
-    method3();
+  public static void method4() {
+    
+    // equals           : 같은 문자열이면 true 반환
+    // equalsIgnoreCase : 같은 문자열이면 true 반환 (대소문자 무시한다.)
+    
+    String str1 = "a";
+    String str2 = "A";
+    
+    System.out.println(str1.equals(str2));
+    System.out.println(str1.equalsIgnoreCase(str2));
+    
   }
 
+  public static void method5() {
+    
+    // length : 문자 개수 반환
+    
+    String str = "hello\n";
+    
+    System.out.println(str.length());
+    
+  }
+
+  public static void method6() {
+    
+    // charAt : 지정한 인덱스(index)의 문자 반환
+    
+    String str = "hello";
+    
+    System.out.println(str.charAt(0));
+    System.out.println(str.charAt(1));
+    System.out.println(str.charAt(2));
+    System.out.println(str.charAt(3));
+    System.out.println(str.charAt(4));
+    
+  }
+  
+  public static void method7() {
+    
+    // indexOf     : 전달한 문자열이 발견된 첫 번째 인덱스를 반환, 없으면 -1을 반환
+    // lastIndexOf : 전달한 문자열이 발견된 마지막  인덱스를 반환, 없으면 -1을 반환
+    
+    String str = "hello world apple";
+    
+    System.out.println(str.indexOf(" "));
+    System.out.println(str.lastIndexOf(" "));
+    
+  }
+  
+  public static void method8() {
+    
+    // substring(int beginIndex)               : beginIndex 부터 끝까지 반환
+    // substring(int beginIndex, int endIndex) : beginIndex 부터 endIndex 이전까지 반환
+    
+    String str = "hello world";
+    
+    int spaceIndex = str.indexOf(" ");
+    
+    System.out.println(str.substring(0, spaceIndex));
+    System.out.println(str.substring(spaceIndex + 1));
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  public static void main(String[] args) {
+    practice01();
+  }
+
+  public static void practice01() {
+    
+    String uri = "http://localhost:8080/project/list.do";
+    String contextPath = "/project";
+    String mapping = uri.substring(uri.indexOf(contextPath) + contextPath.length());
+    
+    System.out.println(mapping);
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
