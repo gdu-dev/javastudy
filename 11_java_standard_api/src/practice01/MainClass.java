e269352f6ed21db0704757d1c90f98d3ce7cd5ed
gdu-dev <minkt78@naver.com>
2024-01-19 12:27:41 +0900

    20240119

--- 11_java_standard_api/src/practice01/MainClass.java
+++ 11_java_standard_api/src/practice01/MainClass.java
@@ -1,6 +1,7 @@
 package practice01;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class MainClass {
 
@@ -10,6 +11,14 @@ public class MainClass {
     // 형식 : 하이픈이 제거된 UUID + 밑줄 + 타임스탬프.확장자
     
     String fileName = "apple.jpg";
+    String extName = fileName.substring(fileName.lastIndexOf("."));
+    StringBuilder builder = new StringBuilder();
+    builder.append(UUID.randomUUID().toString().replace("-", ""));
+    builder.append("_");
+    builder.append(System.currentTimeMillis());
+    builder.append(extName);
+    
+    System.out.println(builder.toString());
     
   }
   
@@ -19,11 +28,37 @@ public class MainClass {
     // 나이 : 현재년도 - 태어난년도
     // 성별 : 하이픈 뒤의 숫자가 1,3이면 남, 2,4이면 여
     
+    return null;
     
+  }
+
+  public static void method3() {
+    
+    // 각 나라별 수도 맞히기
+    
+    // 프랑스 수도는? >>> 
+    // 파리
+    // 스위스 수도는? >>> 
+    // 베른
+    // 영국 수도는? >>> 
+    // 런던
+    // 독일 수도는? >>> 
+    // 베를린
+    // 이탈리아 수도는? >>> 
+    // 로마
+    // 5개 정답 성공
+    
+    Map<String, String> map = Map.of("프랑스", "파리"
+                                   , "독일", "베를린"
+                                   , "이탈리아", "로마"
+                                   , "스위스", "베른"
+                                   , "영국", "런던");
     
   }
   
   public static void main(String[] args) {
+    
+    method1();
     
     Map<String, Object> map = method2("140101-3123456");
     // map을 for문으로 돌려서 확인하기
