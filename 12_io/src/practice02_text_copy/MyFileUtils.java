037df9776140d9bbe39c5cb707816aa14c2b077f
gdu-dev <minkt78@naver.com>
2024-01-24 14:37:18 +0900

    20240124

---
+++ 12_io/src/practice02_text_copy/MyFileUtils.java
@@ -0,0 +1,19 @@
+package practice02_text_copy;
+
+import java.io.File;
+
+public class MyFileUtils {
+
+  public static void fileCopy(File src, File dest) {
+    
+    
+    
+  }
+
+  public static void fileMove(File src, File dest) {
+    
+    
+    
+  }
+  
+}
