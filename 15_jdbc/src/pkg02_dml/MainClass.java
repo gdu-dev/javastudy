ab11a1fed4fbdbf577a64e541bbdeea4d726f4f4
gdu-dev <minkt78@naver.com>
2024-01-29 15:34:22 +0900

    20240129

--- 15_jdbc/src/pkg02_dml/MainClass.java
+++ 15_jdbc/src/pkg02_dml/MainClass.java
@@ -3,6 +3,7 @@ package pkg02_dml;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class MainClass {
 
@@ -57,9 +58,56 @@ public class MainClass {
     }
     
   }
+
+  public static void method2() {
+    
+    Connection con = null;
+    PreparedStatement ps = null;
+    
+    try {
+      
+      Class.forName("oracle.jdbc.OracleDriver");
+      
+      String url = System.getProperty("jdbc.url");
+      String user = System.getProperty("jdbc.user");
+      String password = System.getProperty("jdbc.password");
+      con = DriverManager.getConnection(url, user, password);
+      
+      // 쿼리문에서 변수 처리할 부분은 ?로 처리한다.
+      String sql = "INSERT INTO SAMPLE_T(SAMPLE_NO, SAMPLE_CONTENT, SAMPLE_EDITOR, SAMPLE_DT) VALUES(SAMPLE_SEQ.NEXTVAL, ?, ?, CURRENT_DATE)";
+      
+      ps = con.prepareStatement(sql);
+      
+      // 변수 입력 받기
+      Scanner sc = new Scanner(System.in);
+      System.out.println("내용 입력하세요 >>>");
+      String content = sc.next();
+      System.out.println("편집자 입력하세요 >>>");
+      String editor = sc.next();
+      sc.close();
+      
+      // 변수를 쿼리문에 전달하기
+      ps.setString(1, content);  // 쿼리문의 1번째 물음표에 String content 전달하기
+      ps.setString(2, editor);   // 쿼리문의 2번째 물음표에 String editor 전달하기
+      
+      int result = ps.executeUpdate();
+      System.out.println(result + " 행 이(가) 삽입되었습니다.");
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if(ps != null)  ps.close();
+        if(con != null) con.close();
+      } catch (Exception e2) {
+        e2.printStackTrace();
+      }
+    }
+    
+  }
   
   public static void main(String[] args) {
-    method1();
+    method2();
   }
 
 }
