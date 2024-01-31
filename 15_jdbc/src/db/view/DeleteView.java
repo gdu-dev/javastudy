8ad7f9158f4346850e95946c770b3ef71c3f3189
gdu-dev <minkt78@naver.com>
2024-01-31 11:40:06 +0900

    20240131

--- 15_jdbc/src/db/view/DeleteView.java
+++ 15_jdbc/src/db/view/DeleteView.java
@@ -10,8 +10,8 @@ public class DeleteView implements View {
   public UserDto display() {
     
     String user_no = JOptionPane.showInputDialog("삭제할 사용자의 번호를 입력하세요.");
-    if(JOptionPane.showConfirmDialog(null, "정말 삭제할까요?") == 1) {
-       return null;
+    if(JOptionPane.showConfirmDialog(null, "정말 삭제할까요?") != JOptionPane.OK_OPTION) {
+       return new UserDto(-1, null, null, null);
     }
     
     return new UserDto(Integer.parseInt(user_no), null, null, null);
