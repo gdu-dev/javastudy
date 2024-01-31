f3cc5404cef6eaf5948b3ac56a81adc2e51a0a48
gdu-dev <minkt78@naver.com>
2024-01-31 10:46:19 +0900

    20240131

---
+++ 15_jdbc/src/db/view/ListView.java
@@ -0,0 +1,13 @@
+package db.view;
+
+import db.dto.UserDto;
+
+public class ListView implements View {
+
+  @Override
+  public UserDto display() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ 15_jdbc/src/db/view/RegisterView.java
@@ -0,0 +1,13 @@
+package db.view;
+
+import db.dto.UserDto;
+
+public class RegisterView implements View {
+
+  @Override
+  public UserDto display() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
