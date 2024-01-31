433d1cfd80f61c0cfc1169037e09f2140f7dba85
gdu-dev <minkt78@naver.com>
2024-01-31 09:41:58 +0900

    20240131

--- 15_jdbc/src/db/main/MainClass.java
+++ 15_jdbc/src/db/main/MainClass.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import db.dao.UserDao;
 import db.dto.UserDto;
+import db.service.UserService;
+import db.service.UserServiceImpl;
 
 public class MainClass {
 
   public static void main(String[] args) {
     
     //UserDto userDto = new UserDto(0, "테스트이름", "테스트연락처", null);
-    UserDao userDao = UserDao.getInstance();
+    //UserDao userDao = UserDao.getInstance();
     //int result = userDao.saveUser(userDto);
     //System.out.println(result + "행이 삽입되었습니다.");
 
-    List<UserDto> users = userDao.getUsers();
+    UserService userService = new UserServiceImpl();
+    
+    List<UserDto> users = userService.getUsers();
     for(UserDto user : users) {
       System.out.println(user);
     }
